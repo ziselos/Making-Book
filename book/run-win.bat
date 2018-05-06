b38f39b802455b96cff4b64b2ebc5bf8515ba8b6
Zisis Dimitriadis <zisis.dimitriadis@gmail.com>
2018-05-06 22:11:33 +0300

    Revert "— Minor fixes in Readme"
    This reverts commit 89f9c41ea7c854a104d37cf79000e7d1d90da9aa.

--- book/run-win.bat
+++
@@ -1,9 +0,0 @@
-# Set initial variables.
-bookfolder=book
-set bookName=Zisis
-
-
-pandoc file1.md file2.md file3.md > %bookName%.md
-pandoc -s -o %bookName%.html %bookName%.md
-python -m weasyprint %bookName%.html %bookName%.pdf -s %bookName%.css
-PAUSE
